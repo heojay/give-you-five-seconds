62540156546ff39b17ba14801d96f5054b651b98
Heo Jaewon <yonseijaewon@gmail.com>
2023-02-18 23:51:37 +0900

    Disable dark mode

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{html,js,svelte,ts}'],
-  theme: {
-    extend: {}
-  },
-  plugins: [require("daisyui")],
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+	theme: {
+		extend: {}
+	},
+	plugins: [require('daisyui')],
+	daisyui: {
+		themes: ['light']
+	}
 };
